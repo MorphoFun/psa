da195076de541b5bcb872f564a47d7a6d2794ed3
Sandy Kawano <skawano@nimbios.org>
2016-05-09 16:32:44 -0400

    Edit I_trait for nonlinear gradients

--- R/OpportunityForSelection.R
+++ R/OpportunityForSelection.R
@@ -22,7 +22,7 @@ I_total <- function(fitness, type = c("W", "w")) {
 	return(I_tot)
 }
 
-#' @example
+#' @examples
 # load the dataset
 data(BumpusMales)
 # Calculate the total opportunity for selection
@@ -44,23 +44,24 @@ I_total(BumpusMales$W, type = "W")
 #'
 #' @return \code{I_traits} returns a vector of numeric values. 
 #' 
-#' @section Value: \code{B} A vector of selection gradients (normalized to mean of zero and unit variance)
-#' @section Value: \code{b} slope of a simple linear regression of relative fitness against an individual phenotypic trait.
-#' @section Value: \code{q} Weights to be applied to the phenotypic traits (defaults to 1).
-#' @section Value: \code{var} Imputed variance due to missing data, divided by the weight.
-#' @section Value: \code{s} A vector of selection differentials (normalized to mean of zero and unit variance)
-#' @section Value: \code{i} Additive opportunity for selection for an individual phenotypic trait. 
-#' @section Value: \code{I.model} Opportunity of selection, based on the model of phenotypic traits given. 
-#' @section Value: \code{I.total} The total opportunity for selection, based on weighted variance of fitness.
-#' 
+#' \itemize {
+#'    \code{B} A vector of selection gradients (normalized to mean of zero and unit variance)
+#'    \code{b} slope of a simple linear regression of relative fitness against an individual phenotypic trait.
+#'    \code{q} Weights to be applied to the phenotypic traits (defaults to 1).
+#'    \code{var} Imputed variance due to missing data, divided by the weight.
+#'    \code{s} A vector of selection differentials (normalized to mean of zero and unit variance)
+#'    \code{i} Additive opportunity for selection for an individual phenotypic trait. 
+#'    \code{I.model} Opportunity of selection, based on the model of phenotypic traits given. 
+#'    \code{I.total} The total opportunity for selection, based on weighted variance of fitness.
+#' }
 #' @references Moorad JA, Wade MJ. 1984. Selection gradients, the opportunity for selection, and the coefficient of determination. \emph{The American Naturalist} 181(3): 291-300. \url{http://www.journals.uchicago.edu/doi/abs/10.1086/669158}
 #' @import matrixStats
 #' @import corpcor
 
 require(matrixStats) # for colWeightedMeans
 require(corpcor) # for wt.moments
-I_traits<-function(z, fitness, type = c("W", "w"), wt = NULL){
-  ifelse(type == "w", w <- fitness, w <- fitness/mean(fitness))
+I_traits<-function(z, fitness, wType = c("rel", "abs"), wt = NULL){
+  ifelse(wType == "rel", w <- fitness, w <- fitness/mean(fitness))
   ifelse(is.numeric(wt), wt <- wt, wt <- rep(1, nrow(z)))
   z = data.frame(scale(z), stringsAsFactors = FALSE)
   q = colWeightedMeans(!is.na(z), w=wt)
@@ -82,3 +83,44 @@ I_traits<-function(z, fitness, type = c("W", "w"), wt = NULL){
   unimputed.var = imputed.var/q
   data.frame(B = X$B, b = b, q = q, var = unimputed.var, s = b * q * unimputed.var, i = B * q * unimputed.var * b,
              I.model = sum(B * q * unimputed.var * b, na.rm = TRUE), I.total = wt.moments(x = w,w = wt)$var)}
+
+#' @examples 
+#' 
+
+# load the dataset
+data(BumpusMales)
+
+# Calculate the opportunity for selection for the total variation in fitness and variation from phenotypic traits
+I_traits(BumpusMales[,3:11], BumpusMales$w, fitType = "rel")
+
+
+I_traits<-function(z, fitness, wType = c("rel", "abs"), wt = NULL, ...){
+  ifelse(wType == "rel", w <- fitness, w <- fitness/mean(fitness))
+  ifelse(is.numeric(wt), wt <- wt, wt <- rep(1, nrow(z)))
+  z = data.frame(scale(z), stringsAsFactors = FALSE)
+  q = colWeightedMeans(!is.na(z), w=wt)
+  means = colWeightedMeans(as.matrix(z), w=wt, na.rm = TRUE)
+  n = ncol(z)
+  for (i in 1:n) {z[is.na(z[,i]),i] = means[i]}
+
+  # model for both linear and nonlinear selection gradients
+  model.B <- glam(w, z, "gaussian", prep = FALSE)
+  # compiling all of the selection gradients together, while removing intercepts and linear estimates from the second order model
+  B <- c(model.B$GL$coefficients[-1], model.B$GNL$coefficients[-c(1:(length(z)+1))])
+  
+  X = data.frame(B=B, p = rep(NA,length(B)), name = names(B))
+
+  p <- c(summary(model.B$GL)$coefficients[-1,4], summary(model.B$GNL)$coefficients[-c(1:(length(z)+1)),4])
+  
+  for (i in 1:length(p)) {X$p[which(X$name == names(p[i]))] = p[i] }
+  b = rep(0,n)
+  for (i in 1:n) {
+    model = lm( w ~ z[,i], weights = wt)
+    b[i] = model$coef[-1]
+  }
+  imputed.var = NULL
+  for (i in 1:n) {imputed.var = c(imputed.var,wt.moments(z[,i], w = wt)$var)}
+  unimputed.var = imputed.var/q
+  data.frame(B = X$B, b = b, q = q, var = unimputed.var, s = b * q * unimputed.var, i = B * q * unimputed.var * b,
+             I.model = sum(B * q * unimputed.var * b, na.rm = TRUE), I.total = wt.moments(x = w,w = wt)$var)}
+
