a8b66a80d3299d03c709a934d3d3841f5db0453f
Sandy Kawano <sandy.kawano@gmail.com>
2016-05-23 13:07:19 -0400

    Edit I_traits documentation

--- R/OpportunityForSelection.R
+++ R/OpportunityForSelection.R
@@ -37,23 +37,23 @@ I_total <- function(fitness, type = c("W", "w")) {
 #'
 #' @description Calculation of the opportunity for selection based on the method provided by Moorad and Wade (2013), in which the variation in fitness due to a given trait can be calculated as the product of its selection gradient and selection differential. The I_traits function was written using the code provided in the supplementary material of Moorad and Wade (2013), with slight modifications including a slight correction to  s = b * q * unimputed.var rather than s = B * q * unimputed.var. In addition, the original method by Moorad and Wade (2013) estimated the selection differentials using simple linear regressions that could incorporate weights, but \code{I_traits} estimates the selection differentials using the covariance method (Phillips and Arnold 1983) that does not incorporate weights. The \code{I_traits} function also automatically mean-centers the phenotypic traits. 
 #' 
-#' @usage I_traits(z, fitness, type = c("W", "w"))
+#' @usage I_traits(z, fitness, fitType = c("rel", "abs"))
 #'
 #' @param \code{z} Phenotypic traits. 
 #' @param \code{fitness} Vector of numeric or integer values that represent the fitness metric or proxy.
-#' @param \code{type} Type of fitness metric or proxy, either absolute fitness (\code{"W"}) or relative fitness (\code{"w"}).
+#' @param \code{fitType} Type of fitness metric or proxy, either absolute fitness (\code{"W"}) or relative fitness (\code{"w"}).
 #'
 #' @return \code{I_traits} returns a matrix of numeric values. 
 #' 
-#' \itemize {
-#'    \code{B} A vector of selection gradients (normalized to mean of zero and unit variance)
-#'    \code{b} slope of a simple linear regression of relative fitness against an individual phenotypic trait.
-#'    \code{q} Weights to be applied to the phenotypic traits (defaults to 1).
-#'    \code{var} Imputed variance due to missing data, divided by the weight.
-#'    \code{s} A vector of selection differentials (normalized to mean of zero and unit variance)
-#'    \code{i} Additive opportunity for selection for an individual phenotypic trait. 
-#'    \code{I.model} Opportunity of selection, based on the model of phenotypic traits given. 
-#'    \code{I.total} The total opportunity for selection, based on weighted variance of fitness.
+#' \itemize{
+#'    \item \code{B:} A vector of selection gradients (normalized to mean of zero and unit variance)
+#'    \item \code{b:} slope of a simple linear regression of relative fitness against an individual phenotypic trait.
+#'    \item \code{q:} Weights to be applied to the phenotypic traits (defaults to 1).
+#'    \item \code{var:} Imputed variance due to missing data, divided by the weight.
+#'    \item \code{s:} A vector of selection differentials (normalized to mean of zero and unit variance)
+#'    \item \code{i:} Additive opportunity for selection for an individual phenotypic trait. 
+#'    \item \code{I.model:} Opportunity of selection, based on the model of phenotypic traits given. 
+#'    \item \code{I.total:} The total opportunity for selection, based on weighted variance of fitness.
 #' }
 #' @references Moorad JA, Wade MJ. 2013. Selection gradients, the opportunity for selection, and the coefficient of determination. \emph{The American Naturalist} 181(3): 291-300. \url{http://www.journals.uchicago.edu/doi/abs/10.1086/669158}
 #' @references Phillips PC, Arnold SJ. 1989. Visualizing multivariate selection. \emph{Evolution} 43(6): 1209-1222. \url{http://www.jstor.org/stable/2409357}
