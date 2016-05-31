7209d2b477141cd95a152314dbf1a6247f91174c
Sandy Kawano <sandy.kawano@gmail.com>
2016-05-30 22:59:47 -0400

    add doc for differentials and remove dCompare

--- R/SelectionDifferentials.R
+++ R/SelectionDifferentials.R
@@ -52,236 +52,6 @@ multiprod <- function(x) {
 
 
 
-## From Falconer 1989:
-# selection differential = i*sd(P), which i = intesity of selection and sd(P) is the phenotypic standard deviation
-# the standardized selection differential would then be S/sd(P)
-# See notes about i, though, b/c it depends on normal data
-
-# See Arnold and Wade (1984). On Table 2, they say: "When using Arnold and Wade's (1984) expression (7) to calculate selection differentials, multiply the result by n/(n-1). Expression (7) is for a parameter rather than its statistical estimate."  The equation shown was for both sMean (7a) and sCov (7b). Thus, has the selection differential been miscalculated in the past?
-
-#' @title Comparing estimates of selection differentials
-#'
-#' @name dCompare
-#' 
-#' @description \code{dCompare} allows the user to evaluate how different methods for calculating phenotypic section differentials influence the output and, thus, the interpretation of indirect selection on phenotypic traits. 
-#'
-#' @usage dCompare(w, z)
-#'
-#' @param \code{w} Relative fitness.
-#' @param \code{z} Phenotypic trait(s). Character values are not accepted.
-#'
-#' @section Output:  \code{dCov} is the selection differential calculated from the covariance between the relative fitness, \code{w}, and each phenotypic trait \code{z} (Lande and Arnold 1983).
-#' @section Output:  \code{dMean} is the selection differential calculated as  as described in Brodie et al. (1995). Not output for Gaussian fitness measures.
-#' @section Output:  \code{dMean_stdsd} is the selection differential calculated as described in Gvoždík and Smolinský (2015). Not output for Gaussian fitness measures.
-#' @section Output:  \code{dReg} is the selection differential calculated as the partial regression coefficient of relative fitness, \code{w}, against each individual phenotypic trait through univariate linear regressions.
-#' @section Output:  \code{dCovScale} calculates the selection differential using the equation in \code{dCovScale}, but uses z data that are standardized to a mean of zero and unit variance.
-#' @section Output:  \code{dMeanScale} calculates the selection differential using the equation in \code{dMeanScale}, but uses z and z* data that are standardized to a mean of zero and unit variance. Not output for Gaussian fitness measures.
-#' @section Output:  \code{dRegScale} calculates the selection differential using the equation in \code{dRegScale}, but uses z data that are standardized to a mean of zero and unit variance.
-#'
-#' @return \code{dCompare} returns a matrix of numeric values with 7 rows and X columns, where X = number of phenotpyic traits.
-#'
-#' @references Brodie III ED, Moore AJ, Janzen FJ. 1995. Visualizing and quantifying natural selection. \emph{Trends in Ecology and Evolution} 10(8): 313-318. \url{http://www.sciencedirect.com/science/article/pii/S016953470089117X}
-#' @references Gvoždík L, Smolinský R. 2015. Body size, swimming speed, or thermal sensitivity? Predator-imposed selection on amphibian larvae. \emph{BMC Evolutionary Biology} 15: 238. \url{http://bmcevolbiol.biomedcentral.com/articles/10.1186/s12862-015-0522-y}
-#' @references Lande R, Arnold SJ. 1983. The measurement of selection on correlated characters. \emph{Evolution} 37(6): 1210-1226. \url{http://www.jstor.org/stable/2408842}
-#'
-#' @examples
-#' data(BumpusMales)
-#'
-#' dCompare(BumpusMales$w, BumpusMales[,3:11])
-#' @export
-
-dCompare <- function(w, z) {
-  z <- data.frame(z, stringsAsFactors = FALSE)
-  z2 <- sapply(z, function(x) x^2)
-  ifelse(ncol(z) > 1, colnames(z2) <- paste(names(z), ".Sq", sep=""), colnames(z2) <- paste(names(z), ".Sq", sep=""))
-	df <- cbind(w,z)
-	
-	zScale <- data.frame(scale(z), stringsAsFactors = FALSE)
-	zScales <- data.frame(scale(df[which(df[,1]>0),-1]), stringsAsFactors = FALSE)
-	
-	dfScale <- cbind(w, zScale)
-
-		if(ncol(z) > 1) {
-		  sds <- c(sapply(z, function(x) sd(x)), sapply(data.frame(multiprod(z)), function(x) sd(x)))  
-		  dd <- multiprod(z)
-		  dd_scale <- scale(multiprod(z))
-		  sds_scale <- c(sapply(zScale, function(x) sd(x)), sapply(data.frame(dd_scale), function(x) sd(x))) 
-		} else {
-      dd <- z2
-      }
-		
-
-	# differential based on Covariance (Price equation)
-	if(ncol(z) > 1) {
-	  dCov_linear <- as.numeric(cov(w, z))
-	  dCov_linear_scale <- as.numeric(cov(w, zScale))
-	  z_dev <- sapply(z, function(x) x - mean(x))
-	  dCov_quad <- cov(w, z_dev^2)
-	  dCov_quad_scale <- cov(w, (sapply(data.frame(scale(z)), function(x) x - mean(x)))^2)
-
-	  cpdev <- array(1, c(ncol(z_dev), ncol(z_dev),nrow(z_dev)))
-	  for (i in 1:nrow(z_dev)) {cpdev[,,i] <- t(tcrossprod(as.numeric(z_dev[i,]), as.numeric(z_dev[i,])))}
-	  cpdev.empty.matrix <- matrix(1, nrow=nrow(z_dev), ncol=length(cpdev[,,1][lower.tri(cpdev[,,1])]))
-	  # Pullin the lower triangle of the cp matrix
-	  for (i in 1:nrow(z_dev)) {cpdev.empty.matrix[i,] <- cpdev[,,i][lower.tri(cpdev[,,i])]}
-	  cpdev.paircombos <- data.frame(cpdev.empty.matrix, stringsAsFactors = F)
-	  PairCombos <- combn(colnames(z[,1:ncol(z)]), 2)
-	  PairComboNames <- matrix( "NA", nrow=1, ncol=ncol(PairCombos))
-	  for (i in 1:ncol(PairCombos)) {PairComboNames[,i] <- paste(PairCombos[1,i], "x", PairCombos[2,i])}
-	  colnames(cpdev.paircombos) <- PairComboNames
-	  dCov_corr <- cov(w, cpdev.paircombos)
-	  dCov_corr_scale <- cov(w, data.frame(scale(cpdev.paircombos)))
-	  dCov <- c(dCov_linear, dCov_quad, dCov_corr)
-	  dCovScale <- c(dCov_linear_scale, dCov_quad_scale, dCov_corr_scale)
-	  fullNames <- c(colnames(z), colnames(z2), PairComboNames)
-	  names(dCov) <- fullNames; names(dCovScale) <- fullNames
-  } else {
-	  dCov_linear <- as.numeric(cov(w, z))
-	  dCov_linear_scale <- as.numeric(cov(w, zScale))
-	  
-	  z_dev <- sapply(z, function(x) x - mean(x))
-	  dCov_quad <- cov(w, z_dev^2)
-	  dCov_quad_scale <- cov(w, (sapply(data.frame(scale(z)), function(x) x - mean(x)))^2)
-	  
-	  dCov <- c(dCov_linear, dCov_quad)
-	  names(dCov) <- c(colnames(z), paste(colnames(z), "^2", sep = ""))
-	 
-	  dCovScale <- c(dCov_linear_scale, dCov_quad_scale)
-	  names(dCovScale) <- c(colnames(zScale), paste(colnames(zScale), "^2", sep = ""))             
-	                   
-	}
-
-	  
-	if (length(levels(as.factor(w))) == 2) {
-	# differential based on taking differences between 'before' and 'after' selection events
-	  if(ncol(z) > 1) {
-	    zt <- colMeans(z)
-	    zs <- colMeans(df[which(df[,1]>0),-1])
-	    dMean_linear <- zs-zt
-	    
-	    ztScale <- colMeans(zScale)
-	    zsScale <- colMeans(zScales)
-	    dMean_linear_scale <- zsScale-ztScale
-	    
-	    varzt <- sapply(z, function(x) var(x))
-	    varzs <- sapply(df[which(df[,1]>0),-1], function(x) var(x))
-	    dMean_quad <- varzs - varzt + dMean_linear^2 
-	    
-	    varzt_scale <- sapply(zScale, function(x) var(x))
-	    varzs_scale <- sapply(zScales, function(x) var(x))
-	    dMean_quad_scale <- varzs_scale - varzt_scale + dMean_linear_scale^2 
-	    
-	    covzt <- cov(z)
-	    covzs <- cov(df[which(df[,1]>0),-1])
-	    dMean_corr <- covzs[lower.tri(covzs)] - covzt[lower.tri(covzt)] + tcrossprod(dMean_linear)[lower.tri(tcrossprod(dMean_linear))]
-	    
-	    covzt_scale <- cov(zScale)
-	    covzs_scale <- cov(zScales)
-	    dMean_corr_scale <- covzs_scale[lower.tri(covzs_scale)] - covzt_scale[lower.tri(covzt_scale)] + tcrossprod(dMean_linear_scale)[lower.tri(tcrossprod(dMean_linear_scale))]
-	    
-	    dMean <- c(dMean_linear, dMean_quad, dMean_corr)
-	    names(dMean) <- fullNames
-	    dMean_stdsd_linear <- (zs-zt)/zt
-	    dMean_stdsd_quad <- ((varzs - varzt)/varzt) + dMean_stdsd_linear^2
-	    dMean_stdsd_corr <- ((covzs[lower.tri(covzs)] - covzt[lower.tri(covzt)])/covzt[lower.tri(covzt)]) + tcrossprod(dMean_stdsd_linear)[lower.tri(tcrossprod(dMean_stdsd_linear))]
-	    
-	    dMean_stdsd <- c(dMean_stdsd_linear, dMean_stdsd_quad, dMean_stdsd_corr)
-	    names(dMean_stdsd) <- fullNames
-	    
-	    dMeanScale <- c(dMean_linear_scale, dMean_quad_scale, dMean_corr_scale)
-	    names(dMeanScale) <- fullNames
-	    
-	  } else {
-	    zt <- colMeans(z)
-	    zs <- mean(df[which(df[,1]>0),-1])
-	    dMean_linear <- zs-zt
-	    
-	    ztScale <- colMeans(zScale)
-	    zsScale <- colMeans(zScales)
-	    dMean_linear_scale <- zsScale-ztScale
-	    
-	    varzt <- var(z)
-	    varzs <- var(df[which(df[,1]>0),-1])
-	    dMean_quad <- varzs - varzt + dMean_linear^2 
-	    
-	    varzt_scale <- var(zScale)
-	    varzs_scale <- var(zScales)
-	    dMean_quad_scale <- varzs_scale - varzt_scale + dMean_linear_scale^2 
-	    
-	    dMean <- c(dMean_linear, dMean_quad)
-	    names(dMean) <- c(colnames(z), paste(colnames(z), "^2", sep = ""))
-	    
-	    dMeanScale <- c(dMean_linear_scale, dMean_quad_scale)
-	    names(dMeanScale) <- c(colnames(z), paste(colnames(z), "^2", sep = ""))
-	   
-	    dMean_stdsd_linear <- (zs-zt)/zt
-	    dMean_stdsd_quad <- ((varzs - varzt)/varzt) + dMean_stdsd_linear^2
-	    dMean_stdsd <- c(dMean_stdsd_linear, dMean_stdsd_quad)
-	    names(dMean_stdsd) <- names(dMeanScale)
-	                           
-	  }
-	}
-
-	# nonlinear selection differentials need to be corrected for directional selection; double-check that it is s^2 for quadratic selection, and crossproduct of s1*s2 for correlational selection;
-
-	# differential based on regression
-	if(ncol(z) > 1) {
-	  dReg_linear <- sapply(z, function(x) lm(w ~ x, data = z)$coefficients[2])
-	  names(dReg_linear) <- names(z)
-	  dRegScale_linear <- sapply(zScale, function(x) lm(w ~ x, data = zScale)$coefficients[2])
-	  names(dRegScale_linear) <- names(zScale)
-	  
-	  dReg_quad <- sapply(z, function(x) lm(w ~ x + I(x^2), data = z)$coefficients[3])
-	  names(dReg_quad) <- colnames(z2)
-	  dRegScale_quad <- sapply(zScale, function(x) lm(w ~ x + I(x^2), data = zScale)$coefficients[3])
-	  names(dRegScale_quad) <- colnames(z2)
-	  
-	  PairCombos <- combn(colnames(z[,1:ncol(z)]), 2)
-	  corrmod <- list()
-	  for (i in 1:ncol(PairCombos)) {
-	    corrmod[i] <- paste("w ~", PairCombos[1,i], "*", PairCombos[2,i])
-	  }
-	  
-	  dReg_corrmods <- list()
-	  for (j in 1:length(corrmod)) {
-	    dReg_corrmods[[j]] <-  lm(as.formula(corrmod[[j]]), data = df) 
-	  }
-	  dReg_corr <- sapply(dReg_corrmods, function(x) c(x$coefficients[4]))
-	  dReg <- c(dReg_linear, dReg_quad, dReg_corr) 
-	  names(dReg) <- fullNames
-	  
-	  dRegScale_corrmods <- list()
-	  for (j in 1:length(corrmod)) {
-	    dRegScale_corrmods[[j]] <-  lm(as.formula(corrmod[[j]]), data = dfScale) 
-	  }
-	  dRegScale_corr <- sapply(dRegScale_corrmods, function(x) c(x$coefficients[4]))
-	  dRegScale <- c(dRegScale_linear, dRegScale_quad, dRegScale_corr) 
-	  names(dRegScale) <- fullNames
-	  
-	} else {
-	  dReg_linear <- sapply(z, function(x) lm(w ~ x, data = z)$coefficients[2])
-	  names(dReg_linear) <- names(z)
-	  dRegScale_linear <- sapply(zScale, function(x) lm(w ~ x, data = zScale)$coefficients[2])
-	  names(dRegScale_linear) <- names(zScale)
-	  
-	  dReg_quad <- sapply(z, function(x) lm(w ~ x + I(x^2), data = z)$coefficients[3])
-	  names(dReg_quad) <- colnames(z2)
-	  dRegScale_quad <- sapply(zScale, function(x) lm(w ~ x + I(x^2), data = zScale)$coefficients[3])
-	  names(dRegScale_quad) <- colnames(z2)
-	  
-	  dReg <- c(dReg_linear, dReg_quad)
-	  names(dReg) <- c(colnames(z), paste(colnames(z), "^2", sep = ""))
-	  
-	  dRegScale <- c(dRegScale_linear, dRegScale_quad)
-	  names(dRegScale) <- c(colnames(z), paste(colnames(z), "^2", sep = ""))
-	}
-
-
-	ifelse(length(levels(as.factor(w))) == 2, dAll <- rbind(dCov, dMean, dReg, dMean_stdsd, dCovScale, dMeanScale, dRegScale), dAll <- rbind(dCov, dReg, dCovScale, dRegScale))
-  return(dAll)
-	}
-
-
 #### Calculating the selection differentials, using the covariance method
 #' @title Estimating linear and nonlinear selection differentials
 #'
@@ -347,11 +117,49 @@ dCov <- function(w, z) {
   return(differentials)
 }
 
+## From Falconer 1989:
+# selection differential = i*sd(P), which i = intesity of selection and sd(P) is the phenotypic standard deviation
+# the standardized selection differential would then be S/sd(P)
+# See notes about i, though, b/c it depends on normal data
 
+# See Arnold and Wade (1984). On Table 2, they say: "When using Arnold and Wade's (1984) expression (7) to calculate selection differentials, multiply the result by n/(n-1). Expression (7) is for a parameter rather than its statistical estimate."  The equation shown was for both sMean (7a) and sCov (7b). Thus, has the selection differential been miscalculated in the past?
+
+#' @title Estimate phenotypic selection differentials
+#'
+#' @name differentials
+#' 
+#' @description \code{differentials} allows the user to evaluate how different methods for calculating phenotypic section differentials influence the output and, thus, the interpretation of indirect selection on phenotypic traits. 
+#'
+#' @usage differentials(w, z, method = c(1,2,3,4, "all"), normalize = TRUE, ...)
+#'
+#' @param \code{w} Relative fitness.
+#' @param \code{z} Phenotypic trait(s). Character values are not accepted.
 #' @param \code{method}: method to estimate the selection differential. 1 = covariance of relative fitness to the trait; 2 = differences in mean, variance, and covariance before and after selection; 3 = matrix algebra approach of phenotypic distributions before and after selection; 4 = ordinary least-squares regression of relative fitness against the trait; "all" = use all of the methods to produce multiple estimates. 
+#' @param \code{normalize} Indicate whether phenotypic trait data should be normalized to a mean of zero and unit variance.
+#' @param \code{fitType} 
 #'  
+#'
+#' @section Output:  \code{dCov} is the selection differential calculated from the covariance between the relative fitness, \code{w}, and each phenotypic trait \code{z} (Lande and Arnold 1983).
+#' @section Output:  \code{dBeforeAfter} is the selection differential calculated as  as described in Brodie et al. (1995). 
+#' @section Output: \code{dMatrix} is the selection differential calculated using matrix algebra (e.g., equation 13b in Lande and Arnold (1983))
+#' @section Output:  \code{dReg} is the selection differential calculated as the partial regression coefficient of relative fitness, \code{w}, against each individual phenotypic trait through univariate linear regressions.
+#' @section Output:  \code{dCovScale} calculates the selection differential using the equation in \code{dCovScale}, but uses z data that are standardized to a mean of zero and unit variance.
 
-differentials <- function(w, z, method = c(1,2,3, "all"), normalize = TRUE, fitType = "gaussian", ...) {
+#'
+#' @return \code{differentials} returns a data.frame
+#'
+#' @references Brodie III ED, Moore AJ, Janzen FJ. 1995. Visualizing and quantifying natural selection. \emph{Trends in Ecology and Evolution} 10(8): 313-318. \url{http://www.sciencedirect.com/science/article/pii/S016953470089117X}
+#' @references Lande R, Arnold SJ. 1983. The measurement of selection on correlated characters. \emph{Evolution} 37(6): 1210-1226. \url{http://www.jstor.org/stable/2408842}
+#'
+#' @examples
+#' data(BumpusMales)
+#'
+#' differentials(BumpusMales$w, BumpusMales[,3:11], "all")
+#' @export
+
+
+
+differentials <- function(w, z, method = c(1,2,3,4, "all"), normalize = TRUE, ...) {
   
   isScale <- function(x) {
     ifelse(class(x) == "data.frame", x <- x, x <- data.frame(x, stringsAsFactors = FALSE))
@@ -433,7 +241,7 @@ differentials <- function(w, z, method = c(1,2,3, "all"), normalize = TRUE, fitT
       dReg <- function(w,z) {
         # double-check whether correlational models should have quadratic terms (maybe not)
         d <- cbind(w, z)
-        fitType == fitType
+        fitType == "gaussian"
         dReg_linear <- lm(w ~ ., data = d)
         if (ncol(d) > 2) {
           dReg_nonlinearmod <- glam(w, z, fitType = fitType, prep = FALSE)
@@ -449,15 +257,15 @@ differentials <- function(w, z, method = c(1,2,3, "all"), normalize = TRUE, fitT
       }
       
       if (method == 1) {
-        output <- data.frame(dCov(w,z), check.names = FALSE)
+        output <- data.frame(t(dCov(w,z)), check.names = FALSE)
       } else if(method == 2) {
-        output <- data.frame(dBeforeAfter(w,z), check.names = FALSE)
+        output <- data.frame(t(dBeforeAfter(w,z)), check.names = FALSE)
       } else if(method == 3) {
-        output <- data.frame(dMatrix(w,z), check.names = FALSE)
+        output <- data.frame(t(dMatrix(w,z)), check.names = FALSE)
       } else if(method == 4) {
-        output <- data.frame(dReg(w,z), check.names = FALSE)
+        output <- data.frame(t(dReg(w,z)), check.names = FALSE)
       } else if(method == "all") {
-        output <- data.frame(dCov = dCov(w,z), dBeforeAfter = dBeforeAfter(w,z), dMatrix = dMatrix(w,z), dReg = dReg(w,z))
+        output <- data.frame(t(cbind(dCov = dCov(w,z), dBeforeAfter = dBeforeAfter(w,z), dMatrix = dMatrix(w,z), dReg = dReg(w,z))), row.names = NULL)
       }
       return(output)
 }
