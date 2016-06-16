7f2e81647a1fa9136287117832c4f1d87f465943
Sandy Kawano <sandy.kawano@gmail.com>
2016-06-16 00:35:54 -0400

    edit bootstrap doc

--- R/SelectionDifferentials.R
+++ R/SelectionDifferentials.R
@@ -315,16 +315,16 @@ differentials <- function(w, z, method = c(1,2,3,4, "all"), standardize = TRUE, 
 #' @param \code{conf} Confidence interval. 95% confidence interval is set as a default. See ?boot.ci for more details.
 #' @param \code{R} Number of bootstrap replicates. 2000 is set as the default. See ?boot for more details.
 #' @param \code{method} Method to estimate the selection differential. 1 = covariance of relative fitness to the trait; 2 = differences in mean, variance, and covariance before and after selection; 3 = matrix algebra approach of phenotypic distributions before and after selection; 4 = ordinary least-squares regression of relative fitness against the trait.
-#'
-#' @section Output: \code{bootoutput} contains the estimates for the phenotypic selection differentials, bias, and standard errors using an "ordinary" resampling method (see the "sim" option in boot::boot for more details).
+#' 
+#' @section Output: \code{bootoutput} contains the estimates for the phenotypic selection differentials, bias, and standard errors using an "ordinary" resampling method (see the "sim" option in ?boot for more details).
 #' @section Output: \code{se} contains the bootstrapped standard errors.
-#' @section Output: \code{ci} contains the confidence intervals for four bootstrapping methods (basic, student, percent, and bca). See boot::boot.ci for more details.
-#'
+#' @section Output: \code{ci} contains the confidence intervals for four bootstrapping methods (basic, student, percent, and bca). See ?boot.ci for more details.
+#' 
 #' @return \code{differentials_bootstats} returns a list of three objects (boot output, standard errors, and confidence intervals).
-#'
+#' 
 #' @examples
 #' data(BumpusMales)
-#'
+#' 
 #' differentials_bootstats(BumpusMales$w, scale(BumpusMales[,3:11]), method = 1)
 #' @import boot
 #' @export
